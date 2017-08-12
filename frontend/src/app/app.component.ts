2a239f29c8d3f79564dc7ede0c8e2ea6b0efed8a
Barbi <Manuel.Barbi@adesso.de>
2017-08-12 14:50:45 +0200

    included bootstrap and websocket dependencies

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var SockJS: any;
+declare var Stomp: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  private title: string = 'app';
+  private status: string = 'not connected!';
+
+  ngOnInit() {
+
+  }
+  
 }
